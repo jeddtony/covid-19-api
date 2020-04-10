dd92ee2fcd64c7e1cbe7e8d8cf90de1f2e34c16b
jeddtony <jedtony.jt@gmail.com>
2020-04-10 23:54:17 +0100

    Parsed data to Json

--- services/convertDataToReport.js
+++ services/convertDataToReport.js
@@ -1,13 +1,48 @@
 
+const _ = require('lodash');
+
 const convertDataToJson = (data) => {
     // let splitToArray = data.split('\n');
     let getIndex = data.indexOf('Deaths');
-    let trimString = data.substring(517);
-    console.log(trimString.split('\n                        '));
+    let substring = data.substring(Number(getIndex) + 6);
+    // console.log(trimString.split('\n                        '));
+    substring = substring.split('\n                        ');
+    let trimString = _.filter(substring, (n)=> n !== '')
+    joinToString(trimString);
 }
                
 
 const joinToString = (parsedArray) => {
-    console.log(pa)
+
+    // console.log(parsedArray.toString())
+    let anotherparsed = removeNewLine(parsedArray);
+    console.log(convertArrayToJson(anotherparsed))
+}
+
+const removeNewLine = (parsedArray) => {
+    return parsedArray.filter(parseArr => {
+        if(parseArr.includes('\n')){
+         
+            return
+        }
+      
+        return parseArr;
+    }
+        )
+}
+
+const convertArrayToJson = (parsedArray) => {
+    let convertedArray = []
+    for(i= 0; i< parsedArray.length; i+= 5) {
+       convertedArray.push( {
+            name: parsedArray[i],
+            confirmed: parsedArray[i + 1],
+            admission: parsedArray[i + 2],
+            discharged: parsedArray[i + 3],
+            deaths: parsedArray[i+4]
+        })
+    }
+
+    return convertedArray;
 }
 module.exports = convertDataToJson
